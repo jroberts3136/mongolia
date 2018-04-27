832bf80d8c1a900e1594ee7346ff5656cad016a3
jroberts3136 <Jroberts3136@gmail.com>
2018-04-27 08:48:35 -0400

    I need to have a commit message

--- src/com/company/H.java
+++ src/com/company/H.java
@@ -7,4 +7,31 @@ public class H {
     public static void main(String[] args) {
 
     }
+    public static String f(String f){
+        int ff = (int)(Math.random()*101);
+        if (ff<=49){
+            return
+        }
+        else if (ff>49&&ff<=100){
+            return
+        }
+        else{
+            return
+        }
+    }
+    public static String ff(String f){
+        int ff = (int)(Math.random()*101);
+        if (ff<=49){
+            return
+        }
+        else if (ff>49&&ff<=100){
+            return
+        }
+        else{
+            return
+        }
+    }
+    public static String fff(String f){
+        return
+    }
 }
